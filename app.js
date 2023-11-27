98fca304bedb7cc8a1ef743ce2defc868df26132
divyabasantani1 <151916592+divyabasantani1@users.noreply.github.com>
2023-11-27 21:16:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// Assignment 1: Change Background Color
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+function changeBackgroundColor() {
+  const randomColor = getRandomColor();
+  document.body.style.backgroundColor = randomColor;
+  colorCode.textContent = randomColor;
+}
+
+addEventListener('click', changeBackgroundColor);
